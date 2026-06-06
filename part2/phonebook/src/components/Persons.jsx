e1297cbb909916d75ebaf2e84d64e57b646eda2c
Althea Mae Almedilla <altheamaealmedilla@gmail.com>
2026-06-06 12:24:54 +0800

    Delete part2/phonebook directory

--- part2/phonebook/src/components/Persons.jsx
+++
@@ -1,9 +0,0 @@
-const Persons = ({persons, search}) => {
-  const filteredList = persons.filter(person => person.name.toLowerCase().includes(search.toLowerCase()));
-
-  return (
-    filteredList.map(list => <p key={list.id}>{list.name} {list.number}</p>)
-  )
-}
-
-export default Persons;
