4f60c81c1d487544ab6492f86383a8ba20b87ec4
Althea Almedilla <altheamaealmedilla@gmail.com>
2026-06-11 23:26:02 +0800

    Step 1: Showing data for countries searched.

--- part2/dataforcountries/src/App.jsx
+++ part2/dataforcountries/src/App.jsx
@@ -1,122 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import axios from "axios";
+import { useState, useEffect } from "react";
+import Country from "./components/Country";
+
+const App = () => {
+  const [allCountries, setAllCountries] = useState([]);
+  const [countries, setCountries] = useState([]); 
+  const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    axios
+      .get('https://studies.cs.helsinki.fi/restcountries/api/all')
+      .then(intlCountries => {
+        setAllCountries(intlCountries.data);     
+      })
+      .catch(err => console.log(err));
+  }, []);
+
+  useEffect(() => {
+    if (!search.trim()) {
+      setCountries([]);
+      return;
+    }
+    const filteredCountries = allCountries.filter(c => 
+      c.name?.common?.toLowerCase().includes(search.toLowerCase())
+    );
+    setCountries(filteredCountries);
+  }, [search, allCountries]);
+
+  const handleSearchChange = (event) => {
+    setSearch(event.target.value);
+  };
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+    <div>
+      find country: <input onChange={handleSearchChange} value={search} />
 
-      <div className="ticks"></div>
+      <div className="country">
+        {countries.length === 1 && <Country country={countries[0]} />}
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+        {countries.length > 10 && (
+          <p>Too many matches, specify the country you want to search!</p>
+        )}
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
-}
+        {countries.length > 1 && countries.length <= 10 && (
+          countries.map((country, i) => (
+          <li key={country.cca3 || i}>
+            {country?.name?.common}
+          </li>
+          ))
+        )}
+        
+      </div>  
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ part2/dataforcountries/src/components/Country.jsx
@@ -0,0 +1,31 @@
+
+const Country = ({ country }) => {
+  console.log(country);
+  return (
+    <div>
+      <h2>{country?.name?.common}</h2>
+      <div>
+        <p>
+          <span>Capital: </span>
+          {country?.capital}
+        </p>
+        <p>
+          <span>Area: </span>
+          {country?.area}
+        </p>
+      </div>
+      <div>
+        <h3>languages:</h3>
+        <ul>
+          {Object.values(country?.languages)?.map((lang, i) => (
+            <li key={i}>{lang}</li>
+          ))}
+        </ul>
+
+        <img src={country?.flags?.png} alt={country?.flags?.alt} height={180} />
+      </div>
+    </div>
+  );
+};
+
+export default Country;
