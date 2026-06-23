5926eb29d455a2e3ff2824dedbdc43987934e69b
Althea Almedilla <altheamaealmedilla@gmail.com>
2026-06-23 10:39:55 +0800

    Phonebook Backend step1-2

---
+++ part3/Phonebook-Backend/index.js
@@ -0,0 +1,49 @@
+const { log } = require('console')
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+log('Hello, World')
+
+let persons = [
+  { 
+    "id": "1",
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": "2",
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": "3",
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": "4",
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
+]
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+app.get('/info', (req, res) => {
+  const info = persons.length
+  const reqDate = new Date();
+  res.send(
+    `<p>Phonebook has info for ${info} people</p>
+    <p> ${reqDate.toString()} </p>`
+  )
+})
+
+
+const PORT = 3001
+app.listen(PORT, () => {
+  log(`Server running on port ${PORT}`)
+})
+
