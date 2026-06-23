6e245758bfaf8b5887bb437a0f765978b92e7416
Althea Almedilla <altheamaealmedilla@gmail.com>
2026-06-23 11:36:51 +0800

    Step4 : Deleting resources.

--- part3/Phonebook-Backend/index.js
+++ part3/Phonebook-Backend/index.js
@@ -1,4 +1,4 @@
-const { log } = require('console')
+const { log, error } = require('console')
 const express = require('express')
 const app = express()
 app.use(express.json())
@@ -31,7 +31,7 @@ let persons = [
 // Display all resources
 app.get('/api/persons', (req, res) => {
   res.json(persons)
-})
+});
 
 // Counting the numbers of resources
 app.get('/info', (req, res) => {
@@ -41,7 +41,7 @@ app.get('/info', (req, res) => {
     `<p>Phonebook has info for ${info} people</p>
     <p> ${reqDate.toString()} </p>`
   )
-})
+});
 
 // Display for single entry of resource
 app.get('/api/persons/:id', (req, res) => {
@@ -53,7 +53,22 @@ app.get('/api/persons/:id', (req, res) => {
   }else {
     res.status(404).end()
   }
-})
+});
+
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = req.params.id
+  const person = persons.find((p) => p.id === id);
+
+  if (person) {
+    persons = persons.filter((p) => p.id !== id);
+    res.status(204).end(); 
+    console.log(`${person.name} successfully deleted!`); 
+
+  } else {
+    res.status(404).json({ error: 'Person not found' });
+  }
+});
 
 
 
@@ -61,4 +76,4 @@ app.get('/api/persons/:id', (req, res) => {
 const PORT = 3001
 app.listen(PORT, () => {
   log(`Server running on port ${PORT}`)
-})
+});
