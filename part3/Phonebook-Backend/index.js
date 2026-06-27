5dc950cf9b00c75cf6467899c60527b6699f9e24
Althea Almedilla <altheamaealmedilla@gmail.com>
2026-06-27 12:38:56 +0800

    Step9 : Connected backend and frontend

--- part3/Phonebook-Backend/index.js
+++ part3/Phonebook-Backend/index.js
@@ -2,10 +2,11 @@ const { log, error } = require('console')
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+
 const cors = require('cors')
+app.use(cors())
 
 app.use(express.json())
-app.use(cors())
 
 // Define a custom token to capture POST body data
 morgan.token('body', (req, res) => {
